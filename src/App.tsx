179ae7f2fb610d9b55d48b359ac0731ec6545570
Darren Tham <darrent9859@gmail.com>
2022-07-28 23:01:37 -0400

    Used grid layout for board

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,9 @@ const GRAY_BORDER = '3px solid #bfbfbf'
 
 const App: React.FC = () => {
   const renderBlocks = () => {
-    const rows = []
+    const blocks = []
 
     for (let i = 0; i < NUMS; i++) {
-      const blocks = []
-
       for (let j = 0; j < NUMS; j++) {
         const borderTop = i % SIZE === 0 ? BLACK_BORDER : ''
         const borderLeft = j % SIZE === 0 ? BLACK_BORDER : ''
@@ -21,31 +19,26 @@ const App: React.FC = () => {
         const borderRight = j === (NUMS - 1) ? BLACK_BORDER : ''
         const block = (
           <div
-            key={j}
+            key={i * NUMS + j}
             className='block'
             style={{ borderTop, borderLeft, borderBottom, borderRight }}
           />
         )
         blocks.push(block)
       }
-
-      const row = (
-        <div
-          key={i}
-          style={{ display: 'flex' }}
-        >
-          {blocks.map(block => block)}
-        </div>
-      )
-      rows.push(row)
     }
 
-    return rows
+    return blocks
   }
 
   return (
-    <div>
-      {renderBlocks().map(row => row)}
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${NUMS}, 1fr)`
+      }}
+    >
+      {renderBlocks().map(block => block)}
     </div>
   )
 }
