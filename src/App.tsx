6ca2247a7d4ef832babe565a804e4ea182569777
Darren Tham <darrent9859@gmail.com>
2022-07-29 14:06:24 -0400

    Finished board design

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,101 @@ const SIZE = 3
 const NUMS = SIZE ** 2
 
 const BLACK_BORDER = '3px solid #000000'
-const GRAY_BORDER = '3px solid #bfbfbf'
+const GRAY_BORDER = '1.5px solid #bfbfbf'
+
+interface Border {
+  borderTop: string;
+  borderLeft: string;
+  borderBottom?: string;
+  borderRight?: string;
+}
 
 const App: React.FC = () => {
-  const renderBlocks = () => {
-    const blocks = []
+  const getSectionRows = (): JSX.Element[] => {
+    const rows = []
 
-    for (let i = 0; i < NUMS; i++) {
-      for (let j = 0; j < NUMS; j++) {
-        const borderTop = i % SIZE === 0 ? BLACK_BORDER : ''
-        const borderLeft = j % SIZE === 0 ? BLACK_BORDER : ''
-        const borderBottom = i === (NUMS - 1) ? BLACK_BORDER : ''
-        const borderRight = j === (NUMS - 1) ? BLACK_BORDER : ''
+    for (let i = 0; i < SIZE; i++) {
+      const blocks = []
+
+      for (let j = 0; j < SIZE; j++) {
+        const style: Border = {
+          borderTop: i === 0 ? '' : GRAY_BORDER,
+          borderLeft: j === 0 ? '' : GRAY_BORDER,
+        }
+
         const block = (
           <div
-            key={i * NUMS + j}
+            key={j}
             className='block'
-            style={{ borderTop, borderLeft, borderBottom, borderRight }}
+            style={style}
           />
         )
+
         blocks.push(block)
       }
+
+      const row = (
+        <div
+          key={i}
+          style={{ display: 'flex' }}
+        >
+          {blocks.map(block => block)}
+        </div>
+      )
+
+      rows.push(row)
     }
 
-    return blocks
+    return rows
+  }
+  
+  const getBoardRows = (): JSX.Element[] => {
+    const rows = []
+
+    for (let i = 0; i < SIZE; i++) {
+      const sections = []
+
+      for (let j = 0; j < SIZE; j++) {
+        const sectionStyle: Border = {
+          borderTop: BLACK_BORDER,
+          borderLeft: BLACK_BORDER,
+          borderBottom: i === SIZE - 1 ? BLACK_BORDER : '',
+          borderRight: j === SIZE - 1 ? BLACK_BORDER : '',
+        }
+
+        const section = (
+          <div
+            key={j}
+            className='flex-column'
+            style={sectionStyle}
+          >
+            {getSectionRows().map(sectionRow => sectionRow)}
+          </div>
+        )
+
+        sections.push(section)
+      }
+
+      const row = (
+        <div
+          key={i}
+          style={{ display: 'flex' }}
+        >
+          {sections.map(section => section)}
+        </div>
+      )
+
+      rows.push(row)
+    }
+
+    return rows
   }
 
   return (
     <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${NUMS}, 1fr)`
-      }}
+      className='flex-column'
     >
-      {renderBlocks().map(block => block)}
+      {getBoardRows().map(row => row)}
     </div>
   )
 }
